dcb939d066589a86ed95de1a6f0183cfbf6d3e07
Brian Wu <sonny40920710@gmail.com>
2021-10-28 21:54:47 +0800

    implement TweetPreprocessor

--- 1_NLP_with_classification_and_vector_spaces/preprocessing.py
+++ 1_NLP_with_classification_and_vector_spaces/preprocessing.py
@@ -1,3 +1,5 @@
+from functools import reduce
+from typing import List
 import nltk
 from nltk.corpus import twitter_samples
 import matplotlib.pyplot as plt
@@ -45,12 +47,58 @@ class Tweets(object):
         2. Lowercasing
         3. Removing stop words and punctuation
         4. Stemming (lemmatizing?)
-        5. Othe customized preprocessing steps
+        * Other customized preprocessing steps appropriate for the task
 """
+
+import re
+import string
+
+from nltk.tokenize import TweetTokenizer
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+
+class TweetPreprocessor(object):
+    def __init__(self):
+        self._tokenizer = TweetTokenizer(preserve_case=False, strip_handles=True, reduce_len=True)
+        self._stopwords = set(stopwords.words("english"))
+        self._punctuations = set(string.punctuation)
+        self._stemmer = PorterStemmer()
+
+    # Remove hyperlinks, Twitter marks and styles
+    def customized_preprocess(self, tweet: str) -> str:
+        tweet2 = re.sub(r'^RT[\s]+', '', tweet) # remove old style retweet text "RT"
+        tweet2 = re.sub(r'https?:\/\/.*[\r\n]*', '', tweet2) # remove hyperlinks
+        tweet2 = re.sub(r'#', '', tweet2) # remove hashtags: only removing the hash # sign from the word
+        return tweet2
+
+    def tokenize(self, tweet: str) -> List[str]:
+        return self._tokenizer.tokenize(tweet)
+
+    def remove_sw_punc(self, tokens: List[str]) -> List[str]:
+        clean_tokens = list()
+        for token in tokens:
+            if (token not in self._stopwords) and (token not in self._punctuations):
+                clean_tokens.append(token)
+        return clean_tokens
+    
+    def stem(self, word: str) -> str:
+        return self._stemmer.stem(word)
+    
+    def process_tweet(self, tweet: str) -> List[str]:
+        tweet = self.customized_preprocess(tweet)
+        tokens = self.tokenize(tweet)
+        clean_tokens = self.remove_sw_punc(tokens)
+        stemmed_tokens = list()
+        for clean_token in clean_tokens:
+            stemmed_tokens.append(self.stem(clean_token))
+        return stemmed_tokens
 
 EXAMPLE_TWEET_INDEX = 2277
 
 if __name__ == "__main__":
     tweets = Tweets()
+    preprocessor = TweetPreprocessor()
     example = tweets.get_tweet(pon="pos", index=EXAMPLE_TWEET_INDEX)
-    print(example)
+    print(example, end="\n\n")
+    pp_tokens = preprocessor.process_tweet(example)
+    print(pp_tokens)
