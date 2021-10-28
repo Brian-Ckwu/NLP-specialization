5b2114492ecc2356e297e0be534c24637cdb5076
Brian Wu <sonny40920710@gmail.com>
2021-10-28 10:57:03 +0800

    display tweets information

--- 1_NLP_with_classification_and_vector_spaces/preprocessing.py
+++ 1_NLP_with_classification_and_vector_spaces/preprocessing.py
@@ -1,4 +1,33 @@
 import nltk
+from nltk.corpus import twitter_samples
+import matplotlib.pyplot as plt
+import random
+
+def load_tweets() -> dict:
+    tweets = {
+        "pos": twitter_samples.strings("positive_tweets.json"),
+        "neg": twitter_samples.strings("negative_tweets.json")
+    }
+    return tweets
+
+def display_tweets_info(tweets: dict) -> None:
+    for pon in tweets:
+        print(f"Number of {pon}{'itive' if pon == 'pos' else 'ative'} tweets: {len(tweets[pon])}")
+    print(f"\nThe type of tweets: {type(tweets[pon])}")
+    print(f"The type of a tweet entry: {type(tweets[pon][0])}")
+
+def plot_tweets_ratio(tweets: dict) -> None:
+    plt.figure(figsize=(5, 5))
+    plt.pie(
+        x=[len(tweets[pon]) for pon in tweets],
+        labels=["Positive", "Negative"],
+        autopct="%1.1f%%",
+        startangle=90
+    )
+    plt.axis("equal")
+    plt.show()
 
 if __name__ == "__main__":
-    nltk.download("twitter_samples")
+    tweets = load_tweets()
+    display_tweets_info(tweets)
+    plot_tweets_ratio(tweets)
