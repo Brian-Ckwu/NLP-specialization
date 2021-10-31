133241704d7754b466d3a58491c3d53a53163cae
Brian Wu <sonny40920710@gmail.com>
2021-10-31 18:25:54 +0800

    future work: refactor the codes to a class

--- 1_NLP_with_classification_and_vector_spaces/week1_logistic_regression/training_and_testing.py
+++ 1_NLP_with_classification_and_vector_spaces/week1_logistic_regression/training_and_testing.py
@@ -35,7 +35,7 @@ def evaluate_accuracy(xs: List[str], ys: np.ndarray, freqs: Dict[Tuple[str, floa
 TRAINING_HALF = 4000
 TESTING_HALF = 1000
 LEARNING_RATE = 1e-9
-ITERATIONS = 100
+ITERATIONS = 1000
 
 if __name__ == "__main__":
     t = Tweets()
@@ -44,22 +44,22 @@ if __name__ == "__main__":
     train_xs, train_ys = t.tweets["pos"][:TRAINING_HALF] + t.tweets["neg"][:TRAINING_HALF], np.append(np.ones((TRAINING_HALF, 1)), np.zeros((TRAINING_HALF, 1)), axis=0)
     test_xs, test_ys = t.tweets["pos"][TRAINING_HALF:] + t.tweets["neg"][TRAINING_HALF:], np.append(np.ones(TESTING_HALF), np.zeros(TESTING_HALF))
     freqs = build_freqs(train_xs, train_ys.ravel(), p)
-    # # feature matrix
-    # m = TRAINING_HALF * 2
-    # X = np.zeros((m, 3))
-    # Y = train_ys
-    # print("Extracting features...")
-    # for i, x in tqdm(enumerate(train_xs)):
-    #     X[i, :] = extract_features(x, freqs, p)
-    # # start training
-    # J, theta = gradient_descent(X, Y, theta=np.zeros((3, 1)), alpha=LEARNING_RATE, num_iters=ITERATIONS)
-    # print(J, theta.ravel())
+    # feature matrix
+    m = TRAINING_HALF * 2
+    X = np.zeros((m, 3))
+    Y = train_ys
+    print("Extracting features...")
+    for i, x in tqdm(enumerate(train_xs)):
+        X[i, :] = extract_features(x, freqs, p)
+    # start training
+    J, theta = gradient_descent(X, Y, theta=np.zeros((3, 1)), alpha=LEARNING_RATE, num_iters=ITERATIONS)
+    print(J, theta.ravel())
 
     # load model
-    model = "./models/alpha1e8_iter1000.txt"
-    with open(model) as f:
-        f.readline()
-        theta = np.array(list(map(float, f.readline().split()))).reshape((3, 1))
+    # model = "./models/alpha1e8_iter1000.txt"
+    # with open(model) as f:
+    #     f.readline()
+    #     theta = np.array(list(map(float, f.readline().split()))).reshape((3, 1))
     
     acc = evaluate_accuracy(test_xs, test_ys, freqs, p, theta)
     print(acc)
