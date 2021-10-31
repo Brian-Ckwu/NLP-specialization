ac57133d064a97226324f8588551f81de4a7ff7a
Brian Wu <sonny40920710@gmail.com>
2021-10-31 18:01:41 +0800

    implement predict_tweet()

--- 1_NLP_with_classification_and_vector_spaces/week1_logistic_regression/training_and_testing.py
+++ 1_NLP_with_classification_and_vector_spaces/week1_logistic_regression/training_and_testing.py
@@ -4,7 +4,7 @@ from typing import List, Tuple, Dict
 
 from preprocessing import Tweets, TweetPreprocessor
 from build_frequencies import build_freqs
-from logistic_regression import gradient_descent
+from logistic_regression import sigmoid, gradient_descent
 
 def extract_features(tweet: str, freqs: Dict[Tuple[str, float], int], p: TweetPreprocessor) -> np.ndarray:
     tokens = p.process_tweet(tweet)
@@ -14,6 +14,12 @@ def extract_features(tweet: str, freqs: Dict[Tuple[str, float], int], p: TweetPr
         x[0, 1] += freqs.get((token, 1), 0)
         x[0, 2] += freqs.get((token, 0), 0)
     return x
+
+def predict_tweet(tweet: str, freqs: Dict[Tuple[str, float], int], p: TweetPreprocessor, theta: np.ndarray) -> float:
+    x = extract_features(tweet, freqs, p)
+    z = (x @ theta).item()
+    h = sigmoid(z)
+    return h
 
 TRAINING_HALF = 4000
 TESTING_HALF = 1000
@@ -27,13 +33,23 @@ if __name__ == "__main__":
     train_xs, train_ys = t.tweets["pos"][:TRAINING_HALF] + t.tweets["neg"][:TRAINING_HALF], np.append(np.ones((TRAINING_HALF, 1)), np.zeros((TRAINING_HALF, 1)), axis=0)
     test_xs, test_ys = t.tweets["pos"][TRAINING_HALF:] + t.tweets["neg"][TRAINING_HALF:], np.append(np.ones(TESTING_HALF), np.zeros(TESTING_HALF))
     freqs = build_freqs(train_xs, train_ys.ravel(), p)
-    # feature matrix
-    m = TRAINING_HALF * 2
-    X = np.zeros((m, 3))
-    Y = train_ys
-    print("Extracting features...")
-    for i, x in tqdm(enumerate(train_xs)):
-        X[i, :] = extract_features(x, freqs, p)
-    # start training
-    J, theta = gradient_descent(X, Y, theta=np.zeros((3, 1)), alpha=LEARNING_RATE, num_iters=ITERATIONS)
-    print(J, theta.ravel())
+    # # feature matrix
+    # m = TRAINING_HALF * 2
+    # X = np.zeros((m, 3))
+    # Y = train_ys
+    # print("Extracting features...")
+    # for i, x in tqdm(enumerate(train_xs)):
+    #     X[i, :] = extract_features(x, freqs, p)
+    # # start training
+    # J, theta = gradient_descent(X, Y, theta=np.zeros((3, 1)), alpha=LEARNING_RATE, num_iters=ITERATIONS)
+    # print(J, theta.ravel())
+
+    # load model
+    model = "./models/alpha1e8_iter1000.txt"
+    with open(model) as f:
+        f.readline()
+        theta = np.array(list(map(float, f.readline().split()))).reshape((3, 1))
+    
+    tweet = "I am really happy! Fantastic! :)"
+    prob = predict_tweet(tweet, freqs, p, theta)
+    print(prob)
